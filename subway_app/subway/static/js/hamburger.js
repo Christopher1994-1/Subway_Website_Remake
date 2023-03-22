1fac06628d7fedab0e6562d26669aedac64b5699
CEJ Van Niekirk <cejvanniekirk098@gmail.com>
2023-03-22 15:12:09 -0600

    updates

--- subway_app/subway/static/js/hamburger.js
+++ subway_app/subway/static/js/hamburger.js
@@ -16,36 +16,4 @@ function hamburger_menu_close() {
 
 
 
-
-// Slider Code 
-
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
-
-function showSlide() {
-  // hide all slides
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.remove('active');
-  }
-  // show current slide
-  slides[currentSlide].classList.add('active');
-}
-
-function nextSlide() {
-  currentSlide++;
-  if (currentSlide >= slides.length) {
-    currentSlide = 0;
-  }
-  showSlide();
-}
-
-// show first slide
-showSlide();
-
-// switch slides every 3 seconds
-setInterval(nextSlide, 6000);
-
-
-
-
 // Menu Code
